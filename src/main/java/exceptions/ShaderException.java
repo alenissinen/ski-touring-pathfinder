21961c6ebaf02c903b75579c11aefc05f975a59f
Aleksanteri Nissinen <aeniss@utu.fi>
2026-03-28 18:44:25 +0200

    feat(exceptions): add shader exception

--- src/main/java/exceptions/ShaderException.java
+++ src/main/java/exceptions/ShaderException.java
@@ -1,7 +1,29 @@
 package exceptions;
 
+/**
+ * Thrown when a GLSL shader fails to compile, load or link.
+ *
+ * <p>At least for now extends {@link RuntimeException} because the application
+ * can't render without a working shader. Might change this later e.g. try to recompile
+ * the shader etc. if I figure out a way.</p>
+ */
 public class ShaderException extends RuntimeException {
-  public ShaderException(String message) {
-    super(message);
-  }
+    /**
+     * Constructs a new {@code ShaderException} with a message and the path of the shader.
+     * @param message Error message
+     * @param filePath Path of the shader that caused the error
+     */
+    public ShaderException(String message, String filePath) {
+        super("[file: " + filePath + "]: " + message);
+    }
+
+    /**
+     * Constructs a new {@code ShaderException} with a message and the path of the shader.
+     * @param message Error message
+     * @param filePath Path of the shader that caused the error
+     * @param log OpenGL compiler or linker error log
+     */
+    public ShaderException(String message, String filePath, String log) {
+        super("[file: " + filePath + "]: " + message + "\n" + log);
+    }
 }
